d4e6ead0e46bd528d61a83e309a082f3b60bb83d
SuungYul <pmtf00@gmail.com>
2024-04-19 01:59:49 +0900

    메뉴 모델 구조 변경

--- lib/models/menu_model.dart
+++ lib/models/menu_model.dart
@@ -1,12 +1,6 @@
-class MenuList {
-  final List<Map<String, dynamic>> menuList;
-
-  MenuList.fromJson(Map<String, dynamic> json)
-      : menuList = json["menuQueryDTOList"];
-}
-
 class Menu {
-  final String name;
+  final List<String> names;
+  final List<int> ids;
 
-  Menu.fromJson(Map<String, dynamic> json) : name = json["name"];
+  Menu({required this.names, required this.ids});
 }
