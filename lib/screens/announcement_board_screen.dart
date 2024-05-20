4d071e711c656c2ffddadd20355416f3b6f2e82d
coolman0123 <115522070+coolman0123@users.noreply.github.com>
2024-05-21 08:07:10 +0900

    공지게시판
    조회
    글쓰기
    조회> 갑자기 에러가 뜸

--- lib/screens/announcement_board_screen.dart
+++ lib/screens/announcement_board_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tam_cafeteria_front/screens/view_announcement_screen.dart';
+import 'package:tam_cafeteria_front/screens/write_announce_screen.dart';
+import 'package:tam_cafeteria_front/services/api_service.dart';
 
 class AnnounceBoardScreen extends StatefulWidget {
   const AnnounceBoardScreen({Key? key}) : super(key: key);
@@ -9,13 +11,13 @@ class AnnounceBoardScreen extends StatefulWidget {
 }
 
 class _AnnounceBoardScreenState extends State<AnnounceBoardScreen> {
-  final List<int> _likeCounts = [0, 0, 0, 0, 0]; // 각 공지사항의 좋아요 수를 유지하는 리스트
+  final ApiService _apiService = ApiService();
+  late Future<List<Map<String, dynamic>>> _futureAnnounceList;
 
-  void _incrementLikeCount(int index) {
-    setState(() {
-      // 선택된 공지사항의 좋아요 수를 증가
-      _likeCounts[index]++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _futureAnnounceList = _apiService.fetchBoardList("NOTICE", 1, 1);
   }
 
   @override
@@ -26,7 +28,6 @@ class _AnnounceBoardScreenState extends State<AnnounceBoardScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Expanded(
-              // Expanded로 Row의 자식을 감싸서 중앙 정렬 유지
               child: SizedBox(
                 height: 50,
                 child: Image.asset(
@@ -38,85 +39,101 @@ class _AnnounceBoardScreenState extends State<AnnounceBoardScreen> {
           ],
         ),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              alignment: Alignment.center,
-              height: 56,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(41),
-                color: const Color(0xff002967),
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future: _futureAnnounceList,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            final announceList = snapshot.data!;
+            return SingleChildScrollView(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    height: 56,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(41),
+                      color: const Color(0xff002967),
+                    ),
+                    child: const Text(
+                      '공지 게시판',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  for (int i = 0; i < announceList.length; i++) ...[
+                    buildAnnouncementItem(
+                      context,
+                      index: i,
+                      title: announceList[i]['title'],
+                      publisherName: announceList[i]['publisherName'],
+                      boardId: announceList[i]['id'],
+                    ),
+                    const SizedBox(height: 20.0),
+                  ],
+                ],
               ),
-              child: const Text(
-                '공지 게시판',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            const SizedBox(height: 20.0),
-            // 각 공지 항목에 대한 위젯을 생성하여 반환하는 함수를 호출
-            buildAnnouncementItem(
-              context,
-              index: 0,
-              title: '4월 1주차 식단 채택 목록',
-              content: '짜장면',
-            ),
-            const SizedBox(height: 20.0),
-            buildAnnouncementItem(
-              context,
-              index: 1,
-              title:
-                  '3월 4주차 식단 채택 목록1111111111111111111111111111111111111111111111111111111111111111111111111',
-              content: '짬뽕짬뽕ㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂ',
-            ),
-            const SizedBox(height: 20.0),
-            buildAnnouncementItem(
-              context,
-              index: 2,
-              title: '3월 3주차 식단 채택 목록',
-              content: '스시시ㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣ',
-            ),
-            const SizedBox(height: 20.0),
-            buildAnnouncementItem(
-              context,
-              index: 3,
-              title: '3월 2주차 식단 채택 목록',
-              content:
-                  '물고기, 스테이크, 샐러드, 파스타, 피자, 치킨, 샌드위치, 스시, 스프, 햄버거, 타코, 새우, 소시지, 카레, 떡볶이, 감자튀김, 고기구이, 삼겹살, 닭가슴살, 라면, 김밥, 새우튀김, 치즈, 오믈렛, 라자냐, 쿠키, 아이스크림, ',
-            ),
-          ],
-        ),
+            );
+          }
+        },
       ),
+      floatingActionButton: Builder(
+        builder: (context) {
+          return FloatingActionButton.extended(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const WriteAnnounceScreen(),
+                ),
+              ).then((value) {
+                if (value == true) {
+                  setState(() {
+                    _futureAnnounceList =
+                        _apiService.fetchBoardList("NOTICE", 1, 1);
+                  });
+                }
+              });
+            },
+            icon: Image.asset(
+              'assets/images/write_board_icon.png',
+              width: 70,
+              height: 70,
+            ),
+            label: const Text(''),
+            backgroundColor: Colors.black,
+            shape: const CircleBorder(),
+          );
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 
   Widget buildAnnouncementItem(BuildContext context,
-      {required int index, required String title, required String content}) {
+      {required int index,
+      required String title,
+      required String publisherName,
+      required int boardId}) {
     return GestureDetector(
       onTap: () {
-        // 공지 상세 화면으로 이동
         Navigator.push(
           context,
           MaterialPageRoute(
             builder: (context) => ViewAnnouncement(
-              titles: const [
-                '4월 1주차 식단 채택 목록',
-                '3월 4주차 식단 채택 목록1111111111111111111111111111111111111111111111111111111111111111111111111',
-                '3월 3주차 식단 채택 목록',
-                '3월 2주차 식단 채택 목록'
-              ], // 제목 목록 전달
-              contents: const [
-                '짜장면',
-                '짬뽕짬뽕ㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂ',
-                '스시시ㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣ',
-                '물고기, 스테이크, 샐러드, 파스타, 피자, 치킨, 샌드위치, 스시, 스프, 햄버거, 타코, 새우, 소시지, 카레, 떡볶이, 감자튀김, 고기구이, 삼겹살, 닭가슴살, 라면, 김밥, 새우튀김, 치즈, 오믈렛, 라자냐, 쿠키, 아이스크림, '
-              ], // 내용 목록 전달
+              titles: [title], // 제목 목록 전달
+              contents: [
+                publisherName
+              ], // 발행자 이름 목록 전달 (내용을 publisherName으로 대체)
               currentIndex: index, // 현재 인덱스 전달
             ),
           ),
@@ -150,7 +167,7 @@ class _AnnounceBoardScreenState extends State<AnnounceBoardScreen> {
                     ),
                     const SizedBox(height: 8.0),
                     Text(
-                      content,
+                      ' $publisherName',
                       style: const TextStyle(fontSize: 14.0),
                       overflow: TextOverflow.ellipsis,
                       maxLines: 1,
@@ -160,17 +177,20 @@ class _AnnounceBoardScreenState extends State<AnnounceBoardScreen> {
               ),
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 IconButton(
-                  onPressed: () {
-                    // 좋아요 수 증가
-                    _incrementLikeCount(index);
-                  },
+                  onPressed: () {},
                   icon: const Icon(Icons.thumb_up),
                 ),
-                Text('${_likeCounts[index]}'), // 해당 공지사항의 좋아요 수 표시
-                const SizedBox(width: 15),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 11, 20, 10),
+                  child: Text(
+                    ' $boardId',
+                    style: const TextStyle(fontSize: 14.0),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),
+                ),
               ],
             ),
           ],
